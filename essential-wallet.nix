757479440a6c49997b58ce2eb7d928ad61aa0983
Tom <tomrgowan@gmail.com>
2024-07-04 13:43:19 +1000

    feat: update flake

--- essential-wallet.nix
+++ essential-wallet.nix
@@ -3,9 +3,30 @@
 , stdenv
 , darwin
 , rustPlatform
+, openssl
+, pkg-config
 }:
 let
-  src = ./.;
+  src = builtins.path {
+    path = ./.;
+    filter = path: type:
+      let
+        keepFiles = [
+          "Cargo.lock"
+          "Cargo.toml"
+          "crates"
+        ];
+        includeDirs = [
+          "crates"
+        ];
+        isPathInIncludeDirs = dir: lib.strings.hasInfix dir path;
+      in
+      if lib.lists.any (p: p == (baseNameOf path)) keepFiles then
+        true
+      else
+        lib.lists.any (dir: isPathInIncludeDirs dir) includeDirs
+    ;
+  };
   crateDir = "${src}/crates/wallet";
   crateTOML = "${crateDir}/Cargo.toml";
   lockFile = "${src}/Cargo.lock";
@@ -15,10 +36,19 @@ rustPlatform.buildRustPackage {
   pname = "essential-wallet";
   version = (builtins.fromTOML (builtins.readFile crateTOML)).package.version;
 
+  OPENSSL_NO_VENDOR = 1;
+
+  buildAndTestSubdir = "crates/wallet";
+
   # We run tests separately in CI.
   doCheck = false;
 
-  buildInputs = lib.optionals stdenv.isLinux [
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
   ] ++ lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.SystemConfiguration
   ];
