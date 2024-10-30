ff9a35042012a42b5c3f0640738d32d9ea51a1e6
mitchmindtree <mail@mitchellnordine.com>
2024-10-31 10:12:17 +1100

    feat: Expose `essential-wallet-test` CLI with `test-utils` feature enabled (#5)
    * feat: Expose essential-wallet-test CLI with test-utils feature enabled
    
    * fix: Rename essential-wallet-test exe to essential-wallet-test

--- overlay.nix
+++ overlay.nix
@@ -2,4 +2,10 @@
 # into nixpkgs.
 {}: final: prev: {
   essential-wallet = prev.callPackage ./essential-wallet.nix { };
+  essential-wallet-test = final.essential-wallet.overrideAttrs (finalAttr: prevAttr: {
+    cargoBuildFeatures = [ "test-utils" ];
+    postInstall = ''
+      mv $out/bin/essential-wallet $out/bin/essential-wallet-test
+    '';
+  });
 }
